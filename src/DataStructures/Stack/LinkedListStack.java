6b1f1fbf65491cfebb743494a5af916304fc3a73
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-04 00:43:46 +0200

    Adding Iterator to LinkedListStack Class

--- src/DataStructures/Stack/LinkedListStack.java
+++ src/DataStructures/Stack/LinkedListStack.java
@@ -1,6 +1,8 @@
 package DataStructures.Stack;
 
-public class LinkedListStack<Type> {
+import java.util.Iterator;
+
+public class LinkedListStack<Type> implements Iterable<Type> {
 	private int size;
 	private Node first;
 
@@ -27,6 +29,8 @@ public class LinkedListStack<Type> {
 	}
 
 	public Type pop() {
+		if (size < 0)
+			throw new IndexOutOfBoundsException("No more items in the stack!");
 		Type data = first.data;
 		first = first.next;
 		size--;
@@ -35,5 +39,32 @@ public class LinkedListStack<Type> {
 
 	public int size() {
 		return size;
+	}
+
+	@Override
+	public Iterator<Type> iterator() {
+		return new StackIterator();
+	}
+
+	private class StackIterator implements Iterator<Type> {
+		private Node current = first;
+
+		@Override
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		@Override
+		public Type next() {
+			Type x = current.data;
+			current = current.next;
+			return x;
+		}
+
+		// not allowed
+		@Override
+		public void remove() {
+		}
+
 	}
 }
