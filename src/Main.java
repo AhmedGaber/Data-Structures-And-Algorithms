724f23d9c55302cc133cd7fcb4876f91d0f245d6
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-07 01:24:08 +0200

    Modifing Main class

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,6 @@ public class Main {
 	Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Bag bag = new Bag<Integer>();
-		for (int i = 2; i < 50; i += 2) {
-			bag.add(i);
-		}
-		while (bag.iterator().hasNext())
-			System.out.println(bag.iterator().next());
-
-		System.out.println("=============================");
-
-		ArrayStack stack = new ArrayStack<Integer>();
-		for (int i = 2; i < 50; i += 2) {
-			stack.push(i);
-		}
-		java.util.Iterator i = stack.iterator();
-		while (i.hasNext())
-			System.out.println(i.next());
-
 	}
 
 }
