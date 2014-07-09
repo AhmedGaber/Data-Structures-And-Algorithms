2bd93495928444d8cc3fe9dfde816dd4897dbe87
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-09 16:45:02 +0200

    implementing SelectionSort Class

--- src/Algorithms/Sort/SelectionSort.java
+++ src/Algorithms/Sort/SelectionSort.java
@@ -1,5 +1,24 @@
 package Algorithms.Sort;
 
 public class SelectionSort {
-	
+	public static void sort(Comparable[] a) {
+		for (int i = 0; i < a.length; i++) {
+			int min = i;
+			for (int j = i + 1; j < a.length; j++) {
+				if (less(a[j], a[min]))
+					min = j;
+				exch(a, i, min);
+			}
+		}
+	}
+
+	private static boolean less(Comparable p, Comparable q) {
+		return p.compareTo(q) < 0;
+	}
+
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable k = a[i];
+		a[i] = a[j];
+		a[j] = k;
+	}
 }
