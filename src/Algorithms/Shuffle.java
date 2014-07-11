f5a6bf452b031a71b29186a3e951918db944937e
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-11 20:28:28 +0200

    Shuffle Class Impelmentation done

--- src/Algorithms/Shuffle.java
+++ src/Algorithms/Shuffle.java
@@ -1,5 +1,19 @@
 package Algorithms;
 
+import java.util.Random;
+
 public class Shuffle {
 
+	public static void shuffle(Object[] a) {
+		for (int i = 0; i < a.length; i++) {
+			Random r = new Random(i + 1);
+			exch(a, i, r.nextInt());
+		}
+	}
+
+	private static void exch(Object[] a, int i, int j) {
+		Object k = a[i];
+		a[i] = a[j];
+		a[j] = k;
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -14,20 +14,20 @@ public class Main {
 	Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
-	Comparable<Integer>[] a = new Integer[20];
-	Random r = new Random();
-	System.out.println("Before Sort: ");
-	for (int i = 0; i < a.length; i++) {
-		a[i] = r.nextInt(21);
-		System.out.print(a[i] + " ");
-	}
-	System.out.println("\n" + a.length);
-	new ShellSort().sort(a);
-	System.out.println("\nAfter Sorting");
-	for (int i = 0; i < a.length; i++) {
-		System.out.print(a[i] + " ");
-	}
-	System.out.println("\n" + a.length);
+		Comparable<Integer>[] a = new Integer[20];
+		Random r = new Random();
+		System.out.println("Before Sort: ");
+		for (int i = 0; i < a.length; i++) {
+			a[i] = r.nextInt(21);
+			System.out.print(a[i] + " ");
+		}
+		System.out.println("\n" + a.length);
+		new ShellSort().sort(a);
+		System.out.println("\nAfter Sorting");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println("\n" + a.length);
 	}
 
 }
