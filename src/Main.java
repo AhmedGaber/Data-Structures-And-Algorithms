64f537d15f097c9e466f02a3f16d5ef1bd458012
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-15 01:10:15 +0200

    Testing Deque Class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,6 +8,7 @@ import Algorithms.Sort.SelectionSort;
 import Algorithms.Sort.ShellSort;
 import DataStructures.Bag;
 import DataStructures.Union_Find;
+import DataStructures.Queue.Deque;
 import DataStructures.Stack.ArrayStack;
 
 public class Main {
@@ -16,23 +18,31 @@ public class Main {
 
 	public static void main(String[] args) {
 		Comparable<Integer>[] a = new Integer[20];
-		Random r = new Random();
-		System.out.println("Before Sort: ");
-		for (int i = 0; i < a.length; i++) {
-			a[i] = r.nextInt(21);
-			System.out.print(a[i] + " ");
-		}
-		new ShellSort().sort(a);
-		System.out.println("\nAfter Sorting");
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
-		}
-		new Shuffle().shuffle(a);
-		System.out.println("\nAfter Shuffling");
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
-		}
+		// Random r = new Random();
+		// System.out.println("Before Sort: ");
+		// for (int i = 0; i < a.length; i++) {
+		// a[i] = r.nextInt(21);
+		// System.out.print(a[i] + " ");
+		// }
+		// new ShellSort().sort(a);
+		// System.out.println("\nAfter Sorting");
+		// for (int i = 0; i < a.length; i++) {
+		// System.out.print(a[i] + " ");
+		// }
+		// new Shuffle().shuffle(a);
+		// System.out.println("\nAfter Shuffling");
+		// for (int i = 0; i < a.length; i++) {
+		// System.out.print(a[i] + " ");
+		// }
 
+		Deque<Integer> d = new Deque<>();
+		for (int i = 0; i < 10; i++) {
+			d.addLast(i);
+		}
+		Iterator i = d.iterator();
+		while (i.hasNext())
+			System.out.println(i.next());
+		System.out.println(i.next());
 	}
 
 }
