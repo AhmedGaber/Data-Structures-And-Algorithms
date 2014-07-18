68a84b01613cf156d5c0c339d499f2c8e98d05ba
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-18 13:47:43 +0200

    Implementing LinkedListQueue Class

--- src/DataStructures/Queue/LinkedListQueue.java
+++ src/DataStructures/Queue/LinkedListQueue.java
@@ -1,5 +1,149 @@
 package DataStructures.Queue;
 
-public class LinkedListQueue {
+import java.util.Iterator;
 
+public class LinkedListQueue<Item> implements Iterable<Item> {
+
+    private int size;
+    private Node<Item> first;
+    private Node<Item> last;
+
+    // helper linked list class
+    private class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+        private Node<Item> prev;
+    }
+
+    /**
+     * construct an empty deque
+     */
+    public LinkedListQueue() {
+        this.size = 0;
+        this.first = null;
+        this.last = null;
+    }
+
+    /**
+     * is the deque empty?
+     * 
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * return the number of items on the deque
+     * 
+     * @return
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * insert the item at the front
+     * 
+     * @param item
+     */
+    private void addFirst(Item item) {
+        if (item == null)
+            throw new NullPointerException();
+        if (isEmpty()) {
+            first = new Node<Item>();
+            last = new Node<Item>();
+            first.item = item;
+            last = first;
+        } else {
+            Node<Item> node = new Node<Item>();
+            node.item = item;
+            node.next = first;
+            first.prev = node;
+            node.prev = null;
+            first = node;
+        }
+        size++;
+    }
+
+    /**
+     * insert the item at the end
+     * 
+     * @param item
+     */
+    public void enqueue(Item item) {
+        if (item == null)
+            throw new NullPointerException();
+        if (isEmpty())
+            addFirst(item);
+        else {
+            Node<Item> node = new Node<Item>();
+            node.item = item;
+            node.prev = last;
+            last.next = node;
+            node.next = null;
+            last = node;
+            size++;
+        }
+    }
+
+    /**
+     * delete and return the item at the front
+     * 
+     * @return
+     */
+    public Item dequeue() {
+        if (isEmpty())
+            throw new java.util.NoSuchElementException();
+        else if (size() == 1) {
+            Item item = first.item;
+            first = null;
+            last = null;
+            size--;
+            return item;
+        }
+        Item item = first.item;
+        first = first.next;
+        first.prev = null;
+        size--;
+        return item;
+    }
+
+    /**
+     * return an iterator over items in order from front to end
+     */
+    @Override
+    public Iterator<Item> iterator() {
+        return new QueueIterator();
+    }
+
+    private class QueueIterator implements Iterator<Item> {
+        private Node<Item> current;
+
+        public QueueIterator() {
+            this.current = first;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (first == null)
+                return false;
+            return !(current == null);
+        }
+
+        @Override
+        public Item next() {
+            if (!hasNext())
+                throw new java.util.NoSuchElementException();
+            Item x = current.item;
+            current = current.next;
+            return x;
+        }
+
+        // not allowed
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,40 +9,40 @@ import Algorithms.Sort.ShellSort;
 import DataStructures.Bag;
 import DataStructures.Union_Find;
 import DataStructures.Queue.Deque;
+import DataStructures.Queue.LinkedListQueue;
 import DataStructures.Stack.ArrayStack;
 
 public class Main {
 
-	static Union_Find UF = new Union_Find(10);
-	Scanner in = new Scanner(System.in);
+    static Union_Find UF = new Union_Find(10);
+    Scanner in = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Comparable<Integer>[] a = new Integer[20];
-		// Random r = new Random();
-		// System.out.println("Before Sort: ");
-		// for (int i = 0; i < a.length; i++) {
-		// a[i] = r.nextInt(21);
-		// System.out.print(a[i] + " ");
-		// }
-		// new ShellSort().sort(a);
-		// System.out.println("\nAfter Sorting");
-		// for (int i = 0; i < a.length; i++) {
-		// System.out.print(a[i] + " ");
-		// }
-		// new Shuffle().shuffle(a);
-		// System.out.println("\nAfter Shuffling");
-		// for (int i = 0; i < a.length; i++) {
-		// System.out.print(a[i] + " ");
-		// }
+    public static void main(String[] args) {
+        Comparable<Integer>[] a = new Integer[20];
+        // Random r = new Random();
+        // System.out.println("Before Sort: ");
+        // for (int i = 0; i < a.length; i++) {
+        // a[i] = r.nextInt(21);
+        // System.out.print(a[i] + " ");
+        // }
+        // new ShellSort().sort(a);
+        // System.out.println("\nAfter Sorting");
+        // for (int i = 0; i < a.length; i++) {
+        // System.out.print(a[i] + " ");
+        // }
+        // new Shuffle().shuffle(a);
+        // System.out.println("\nAfter Shuffling");
+        // for (int i = 0; i < a.length; i++) {
+        // System.out.print(a[i] + " ");
+        // }
 
-		Deque<Integer> d = new Deque<>();
-		for (int i = 0; i < 10; i++) {
-			d.addLast(i);
-		}
-		Iterator i = d.iterator();
-		while (i.hasNext())
-			System.out.println(i.next());
-		System.out.println(i.next());
-	}
+        LinkedListQueue<Integer> d = new LinkedListQueue<>();
+        for (int i = 0; i < 10; i++) {
+            d.enqueue(i);
+        }
+        Iterator i = d.iterator();
+        while (i.hasNext())
+            System.out.println(i.next());
+    }
 
 }
