3529eddbed1f3386d515404ae233a769b50fa3bd
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-22 04:26:12 +0200

    Improving MergSort

--- src/Algorithms/Sort/MergSort.java
+++ src/Algorithms/Sort/MergSort.java
@@ -13,6 +13,8 @@ public class MergSort {
         int mid = lo + (hi - lo) / 2;
         sort(a, aux, lo, mid);
         sort(a, aux, mid + 1, hi);
+        if (!less(a[mid + 1], a[mid]))
+            return;
         merge(a, aux, lo, mid, hi);
     }
 
