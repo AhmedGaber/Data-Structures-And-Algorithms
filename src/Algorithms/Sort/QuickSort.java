7d2fae572553f747d6fa05445eea73722ce8e23f
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-23 19:54:42 +0200

    Implementing QuickSort Class

--- src/Algorithms/Sort/QuickSort.java
+++ src/Algorithms/Sort/QuickSort.java
@@ -1,5 +1,47 @@
 package Algorithms.Sort;
 
+import Algorithms.Shuffle;
+
 public class QuickSort {
+
+    public static void sort(Comparable[] a) {
+        new Shuffle().shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo)
+            return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        while (true) {
+            while (less(a[++i], a[lo]))
+                if (i == hi)
+                    break;
+            while (less(a[lo], a[--j]))
+                if (j == lo)
+                    break;
+            if (i >= j)
+                break;
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    private static boolean less(Comparable p, Comparable q) {
+        return p.compareTo(q) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable k = a[i];
+        a[i] = a[j];
+        a[j] = k;
+    }
 
 }
