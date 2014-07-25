fea0623cc297d91ae39fe3c98b148ec1732ff094
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-25 18:53:37 +0200

    Doocumenting QuickSort Class

--- src/Algorithms/Sort/QuickSort.java
+++ src/Algorithms/Sort/QuickSort.java
@@ -2,13 +2,27 @@ package Algorithms.Sort;
 
 import Algorithms.Shuffle;
 
+/**
+ * 
+ * @author 3Arrows
+ * 
+ */
 public class QuickSort {
-
+    /**
+     * 
+     * @param a
+     */
     public static void sort(Comparable[] a) {
         new Shuffle().shuffle(a);
         sort(a, 0, a.length - 1);
     }
 
+    /**
+     * 
+     * @param a
+     * @param lo
+     * @param hi
+     */
     private static void sort(Comparable[] a, int lo, int hi) {
         if (hi <= lo)
             return;
@@ -17,6 +31,13 @@ public class QuickSort {
         sort(a, j + 1, hi);
     }
 
+    /**
+     * 
+     * @param a
+     * @param lo
+     * @param hi
+     * @return
+     */
     private static int partition(Comparable[] a, int lo, int hi) {
         int i = lo, j = hi + 1;
         while (true) {
@@ -34,10 +55,22 @@ public class QuickSort {
         return j;
     }
 
+    /**
+     * 
+     * @param p
+     * @param q
+     * @return
+     */
     private static boolean less(Comparable p, Comparable q) {
         return p.compareTo(q) < 0;
     }
 
+    /**
+     * 
+     * @param a
+     * @param i
+     * @param j
+     */
     private static void exch(Comparable[] a, int i, int j) {
         Comparable k = a[i];
         a[i] = a[j];
