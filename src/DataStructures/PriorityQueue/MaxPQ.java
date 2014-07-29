965c8e47d282df8b866ac6bd7d3356493699b248
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-29 04:27:59 +0200

    Adding the required interface to the PQ Package

--- src/DataStructures/PriorityQueue/MaxPQ.java
+++ src/DataStructures/PriorityQueue/MaxPQ.java
@@ -1,5 +1,32 @@
 package DataStructures.PriorityQueue;
 
-public class MaxPQ {
+public class MaxPQ<Key extends Comparable<Key>> {
+    public void MaxPQ() {
 
+    }
+
+    public void MaxPQ(Key[] a) {
+
+    }
+
+    public void insert(Key v) {
+
+    }
+
+    public Key delMax() {
+        // modify that
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public Key max() {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
 }
--- src/DataStructures/PriorityQueue/MinPQ.java
+++ src/DataStructures/PriorityQueue/MinPQ.java
@@ -1,5 +1,32 @@
 package DataStructures.PriorityQueue;
 
-public class MinPQ {
+public class MinPQ<Key extends Comparable<Key>> {
+    public void MinPQ() {
 
+    }
+
+    public void MinPQ(Key[] a) {
+
+    }
+
+    public void insert(Key v) {
+
+    }
+
+    public Key delMin() {
+        // modify that
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public Key min() {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
 }
