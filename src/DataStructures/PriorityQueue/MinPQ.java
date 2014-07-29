8f11027a2809f7a21127a1201e1677d6f85243d9
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-29 04:51:09 +0200

    Implementing MinPQ qnd MaxPQ Classes

--- src/DataStructures/PriorityQueue/MinPQ.java
+++ src/DataStructures/PriorityQueue/MinPQ.java
@@ -1,32 +1,71 @@
 package DataStructures.PriorityQueue;
 
 public class MinPQ<Key extends Comparable<Key>> {
-    public void MinPQ() {
+    private Key[] pq;
+    private int N;
+    private int next;
 
+    public void MinPQ() {
+        pq = (Key[]) new Comparable[2];
     }
 
     public void MinPQ(Key[] a) {
-
+        this.pq = a;
     }
 
     public void insert(Key v) {
-
+        if (N == pq.length)
+            resize(pq.length * 2);
+        pq[++N] = v;
+        swim(N);
     }
 
     public Key delMin() {
+        if (N > 0 && N == pq.length / 4)
+            resize(pq.length / 2);
         // modify that
         return null;
     }
 
     public boolean isEmpty() {
-        return false;
+        return N == 0;
     }
 
     public Key min() {
-        return null;
+        return pq[0];
     }
 
     public int size() {
-        return 0;
+        return N;
+    }
+
+    private void swim(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            exch(k, k / 2);
+            k = k / 2;
+        }
+
+    }
+
+    private void sink(int k) {
+
+    }
+
+    private boolean greater(int i, int j) {
+        return pq[i].compareTo(pq[j]) > 0;
+    }
+
+    private void exch(int i, int j) {
+        Key temp = pq[i];
+        pq[i] = pq[j];
+        pq[j] = temp;
+    }
+
+    private void resize(int capacity) {
+        Key[] temp = (Key[]) new Object[capacity];
+        for (int i = 0; i < next; i++) {
+            temp[i] = pq[i];
+        }
+        this.pq = temp;
     }
 }
