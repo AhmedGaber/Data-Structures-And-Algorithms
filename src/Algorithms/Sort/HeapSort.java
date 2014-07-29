33995f7b1e5cddece39727b2603c847e99eca11d
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-29 05:14:49 +0200

    Implementig HeapSort Class

--- src/Algorithms/Sort/HeapSort.java
+++ src/Algorithms/Sort/HeapSort.java
@@ -1,5 +1,36 @@
 package Algorithms.Sort;
 
 public class HeapSort {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int K = N / 2; K >= 1; K--)
+            sink(a, K, N);
+        while (N > 1) {
+            exch(a, 1, N);
+            sink(a, 1, --N);
+        }
+    }
+
+    private static void sink(Comparable[] a, int k, int N) {
+        while (2 * k <= N) {
+            int j = 2 * k;
+            if (j < N && less(a, j, j + 1))
+                j++;
+            if (!less(a, k, j))
+                break;
+            exch(a, k, j);
+            k = j;
+        }
+    }
+
+    private static boolean less(Comparable[] a, int i, int j) {
+        return a[i].compareTo(a[j]) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 
 }
