13e9e607e60aee1caa0dd997b9ed89c52d574b05
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-31 03:09:01 +0200

    Implemented Node, put(),and  get() in BST Class

--- src/DataStructures/BinarySearchTree/BST.java
+++ src/DataStructures/BinarySearchTree/BST.java
@@ -1,5 +1,7 @@
 package DataStructures.BinarySearchTree;
 
+import org.w3c.dom.NodeList;
+
 /**
  * An Implementation of the Binary Search Tree
  * 
@@ -11,7 +13,23 @@ package DataStructures.BinarySearchTree;
 public class BST<Key extends Comparable<Key>, Value> {
     private Node root;
 
+    /**
+     * Node is the basic structure of the tree.
+     * 
+     * @author Ahmed Gaber
+     * 
+     */
     private class Node {
+        private Key key;
+        private Value value;
+        private Node left;
+        private Node right;
+        private int couunt; // number of nodes in this node's subtree
+
+        public Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 
     /**
@@ -20,7 +38,21 @@ public class BST<Key extends Comparable<Key>, Value> {
      * @param key
      * @param val
      */
-    public void put(Key key, Value val) {
+    public void put(Key key, Value value) {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node node, Key key, Value value) {
+        if (node == null)
+            return new Node(key, value);
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = put(node.left, key, value);
+        else if (cmp > 0)
+            node.right = put(node.right, key, value);
+        else
+            node.value = value;
+        return node;
     }
 
     /**
@@ -30,7 +62,17 @@ public class BST<Key extends Comparable<Key>, Value> {
      * @return
      */
     public Value get(Key key) {
-
+        Node node = root;
+        while (node != null) {
+            int cmp = key.compareTo(node.key);
+            if (cmp < 0)
+                node = node.left;
+            else if (cmp > 0)
+                node = node.right;
+            else
+                return node.value;
+        }
+        return null;
     }
 
     /**
