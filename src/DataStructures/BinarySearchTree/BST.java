09f8eb10dc54e51ec28cfb9e121f250b55af9685
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-31 03:34:16 +0200

    Implemented  delete(), min(), max(), deleteMin(), deleteMax(), and size() methods in the BST Class

--- src/DataStructures/BinarySearchTree/BST.java
+++ src/DataStructures/BinarySearchTree/BST.java
@@ -1,7 +1,5 @@
 package DataStructures.BinarySearchTree;
 
-import org.w3c.dom.NodeList;
-
 /**
  * An Implementation of the Binary Search Tree
  * 
@@ -24,7 +22,7 @@ public class BST<Key extends Comparable<Key>, Value> {
         private Value value;
         private Node left;
         private Node right;
-        private int couunt; // number of nodes in this node's subtree
+        private int count; // number of nodes in this node's subtree
 
         public Node(Key key, Value value) {
             this.key = key;
@@ -52,6 +50,7 @@ public class BST<Key extends Comparable<Key>, Value> {
             node.right = put(node.right, key, value);
         else
             node.value = value;
+        node.count = 1 + size(node.left) + size(node.right);
         return node;
     }
 
@@ -81,6 +80,60 @@ public class BST<Key extends Comparable<Key>, Value> {
      * @param key
      */
     public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, Key key) {
+        if (node == null)
+            return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = delete(node.left, key);
+        else if (cmp > 0)
+            node.right = delete(node.right, key);
+        else {
+            if (node.right == null)
+                return node.left;
+            if (node.left == null)
+                return node.right;
+
+            Node temp = node;
+            node = min(temp.right);
+            node.right = deleteMin(temp.right);
+            node.left = temp.left;
+        }
+        node.count = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    /**
+     * Returns the node with the minimum value
+     * 
+     * @return
+     */
+    public Node min() {
+        return min(root);
+    }
+
+    private Node min(Node node) {
+        if (node.left == null)
+            return node;
+        return min(node);
+    }
+
+    /**
+     * Returns the node with the maximum value
+     * 
+     * @return
+     */
+    public Node max() {
+        return max(root);
+    }
+
+    private Node max(Node node) {
+        if (node.right == null)
+            return node;
+        return min(node);
     }
 
     /**
@@ -90,4 +143,56 @@ public class BST<Key extends Comparable<Key>, Value> {
      */
     public Iterable<Key> iterator() {
     }
+
+    /**
+     * Returns number of nodes in the tree
+     * 
+     * @return
+     */
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null)
+            return 0;
+        return node.count;
+    }
+
+    /**
+     * Deletes the node with the minimum value from the tree
+     * 
+     * @param node
+     * @return
+     */
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node node) {
+        if (node.left == null)
+            return node.right;
+        node.left = deleteMin(node.left);
+        node.count = 1 + size(node.left) + size(node.right);
+        return node;
+    }
+
+    /**
+     * Deletes the node with the maximum value from the tree
+     * 
+     * @param node
+     * @return
+     */
+    public void deleteMax() {
+        root = deleteMax(root);
+    }
+
+    private Node deleteMax(Node node) {
+        if (node.right == null)
+            return node.left;
+        node.right = deleteMin(node.right);
+        node.count = 1 + size(node.right) + size(node.left);
+        return node;
+    }
+
 }
