6adcc870a944fa7d3eea939549a813afdf8e3c8c
AhmedGaber <ahmedgaber20@gmail.com>
2014-07-31 02:49:50 +0200

    Writing main methods that BST needs to provide

--- src/DataStructures/BinarySearchTree/BST.java
+++ src/DataStructures/BinarySearchTree/BST.java
@@ -1,5 +1,51 @@
 package DataStructures.BinarySearchTree;
 
-public class BST {
+/**
+ * An Implementation of the Binary Search Tree
+ * 
+ * @author Ahmed Gaber
+ * 
+ * @param <Key>
+ * @param <Value>
+ */
+public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
 
+    private class Node {
+    }
+
+    /**
+     * Associates value with key.
+     * 
+     * @param key
+     * @param val
+     */
+    public void put(Key key, Value val) {
+    }
+
+    /**
+     * Returns value corresponding to given key, or null if no such key
+     * 
+     * @param key
+     * @return
+     */
+    public Value get(Key key) {
+
+    }
+
+    /**
+     * Deletes a value from the tree
+     * 
+     * @param key
+     */
+    public void delete(Key key) {
+    }
+
+    /**
+     * Returns an Iterator over all values in the tree
+     * 
+     * @return
+     */
+    public Iterable<Key> iterator() {
+    }
 }
